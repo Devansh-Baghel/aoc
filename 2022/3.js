8dd2ff18b7127a9186e0d3061c9785e05ac5b18d
Devansh Baghel <devanshbaghel85@gmail.com>
2023-11-26 17:32:25 +0530

    solved part 2 day 3

--- 2022/3.js
+++ 2022/3.js
@@ -1,41 +1,64 @@
-const fs = require("node:fs")
+const fs = require("node:fs");
 
 fs.readFile("./3.txt", "utf8", (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
 
-  data = data.trim()
-  data = data.split("\n")
+  data = data.trim();
+  data = data.split("\n");
 
-  let result = []
+  let result = [];
 
   for (let i in data) {
-    const comp1 = data[i].slice(0, (data[i].length)/2)
-    const comp2 = data[i].slice((data[i].length)/2, data[i].length)
+    const comp1 = data[i].slice(0, data[i].length / 2);
+    const comp2 = data[i].slice(data[i].length / 2, data[i].length);
 
-    for (let j in comp1){
-      if (comp2.includes(comp1[j])){
-        result.push(comp1[j])
-        break
+    for (let j in comp1) {
+      if (comp2.includes(comp1[j])) {
+        result.push(comp1[j]);
+        break;
       }
     }
   }
 
-  let final = []
+  function getPrioritySum(arr) {
+    let final = [];
 
-  for (let i of result) {
-    if (i.toUpperCase() === i) {
-      final.push(i.charCodeAt(0) - 38)
-    } else {
-      final.push(i.charCodeAt(0) - 96)
+    for (let i of arr) {
+      if (i.toUpperCase() === i) {
+        final.push(i.charCodeAt(0) - 38);
+      } else {
+        final.push(i.charCodeAt(0) - 96);
+      }
+    }
+
+    const sum = final.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+
+    return sum
+  }
+
+  console.log(getPrioritySum(result))
+
+  // Part 2
+
+  let result2 = [];
+  for (let i = 0; i < 300; i++) {
+    if (i % 3 !== 0) continue;
+    for (let j in data[i]) {
+      if (
+        data[i + 1].includes(data[i][j]) &&
+        data[i + 2].includes(data[i][j])
+      ) {
+        result2.push(data[i][j]);
+        break;
+      }
     }
   }
 
-  const sum = final.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue
-  },0);
-
-  console.log(sum)
-})
+  console.log(result2.length);
+  console.log(getPrioritySum(result2))
+});
