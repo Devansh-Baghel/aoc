b29015abd7c54bca252999abd35474e67dce72e7
Devansh Baghel <devanshbaghel85@gmail.com>
2023-12-02 15:41:43 +0530

    Day 02 - Part 2

--- 2023/2/2.js
+++ 2023/2/2.js
@@ -4,14 +4,17 @@ data.pop();
 
 // Part 1
 
-let result = 0;
+let part1 = 0;
+let part2 = 0;
 
 for (let i in data) {
   const red = Math.max(...data[i].match(/(\d+)\s+red/g).map(elm => parseInt(elm.match(/\d+/)[0], 10)))
   const green = Math.max(...data[i].match(/(\d+)\s+green/g).map(elm => parseInt(elm.match(/\d+/)[0], 10)))
   const blue = Math.max(...data[i].match(/(\d+)\s+blue/g).map(elm => parseInt(elm.match(/\d+/)[0], 10)))
 
-  if (red <= 12 && green <= 13 && blue <= 14) result += parseInt(i) + 1
+  if (red <= 12 && green <= 13 && blue <= 14) part1 += parseInt(i) + 1
+  part2 += red * green * blue
 }
 
-console.log(result)
+console.log(part1)
+console.log(part2)
